67f1d7c6abe9497811fecd81612a45c0ab4e3d3b
jjabides <jj.abides@gmail.com>
2026-02-06 13:33:18 -0500

    fix more linting issues

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -117,5 +117,6 @@ def initiate():
         CarModel.objects.create(
             name=data['name'],
             car_make=data['car_make'],
-            type=data['type'],year=data['year']
+            type=data['type'],
+            year=data['year']
         )
