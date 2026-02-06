5a97351227f8e36ef4cdf2ae349500d0011646ad
jjabides <jj.abides@gmail.com>
2026-02-06 14:11:17 -0500

    fix linting issues

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,13 +1,5 @@
 # Uncomment the required imports before adding the code
 
-from django.shortcuts import render
-from django.http import HttpResponseRedirect, HttpResponse
-from django.contrib.auth.models import User
-from django.shortcuts import get_object_or_404, render, redirect
-from django.contrib.auth import logout
-from django.contrib import messages
-from datetime import datetime
-
 from django.http import JsonResponse
 from django.contrib.auth import login, authenticate
 import logging
@@ -51,18 +43,7 @@ def get_cars(request):
         cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
     return JsonResponse({"CarModels":cars})
 
-# Create a `logout_request` view to handle sign out request
-# def logout_request(request):
-# ...
 
-# Create a `registration` view to handle sign up request
-# @csrf_exempt
-# def registration(request):
-# ...
-
-# # Update the `get_dealerships` view to render the index page with
-# a list of dealerships
-#Update the `get_dealerships` render list of dealerships all by default, particular state if state is passed
 def get_dealerships(request, state="All"):
     if(state == "All"):
         endpoint = "/fetchDealers"
@@ -71,7 +52,7 @@ def get_dealerships(request, state="All"):
     dealerships = get_request(endpoint)
     return JsonResponse({"status":200,"dealers":dealerships})
 
-# Create a `get_dealer_reviews` view to render the reviews of a dealer
+
 def get_dealer_reviews(request,dealer_id):
     if (dealer_id):
         endpoint = f"/fetchReviews/dealer/{dealer_id}"
@@ -84,7 +65,7 @@ def get_dealer_reviews(request,dealer_id):
     else:
         return JsonResponse({"status":400, "message": "Bad Request"})
 
-# Create a `get_dealer_details` view to render the dealer details
+
 def get_dealer_details(request, dealer_id):
     if (dealer_id):
         endpoint = f"/fetchDealer/{dealer_id}"
@@ -93,7 +74,7 @@ def get_dealer_details(request, dealer_id):
     else:
         return JsonResponse({"status":400, "message": "Bad Request"})
 
-# Create a `add_review` view to submit a review
+
 def add_review(request):
     if(request.user.is_anonymous == False):
         data = json.loads(request.body)
