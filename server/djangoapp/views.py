5b93c7ee495a68e21a1d1709cda468156836861f
jjabides <jj.abides@gmail.com>
2026-02-06 14:21:03 -0500

    fix linting issues

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -85,7 +85,7 @@ def add_review(request):
             return JsonResponse({"status": 200})
         except Exception:
             return JsonResponse({
-                "status": 401, 
+                "status": 401,
                 "message": "Error in posting review"
             })
     else:
