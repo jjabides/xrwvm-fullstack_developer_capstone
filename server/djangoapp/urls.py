da949608f49af03750412e2463e7be367b402955
jjabides <jj.abides@gmail.com>
2026-02-06 21:28:47 -0500

    update register url path

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -6,12 +6,11 @@ from . import views
 
 app_name = 'djangoapp'
 urlpatterns = [
-    # # path for registration
 
     # path for login
     path(route='login', view=views.login_user, name='login'),
     path(route='logout', view=views.logout_request, name='logout'),
-    path(route='registration', view=views.registration, name='registration'),
+    path(route='register', view=views.registration, name='register'),
     path(route='get_cars', view=views.get_cars, name='getcars'),
 
     # path for dealer reviews view
